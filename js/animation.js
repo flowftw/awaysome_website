dcbc57cfdc75ec8e1e3ea1382ff8cb663a04bd5c
Florian Schumann <florian@gartenstrasse7.de>
2019-05-18 17:09:49 +0200

    animate header.

---
+++ js/animation.js
@@ -0,0 +1,13 @@
+function animate() {
+    anime({
+        targets: '.header path',
+        strokeDashoffset: [anime.setDashoffset, 0],
+        easing: 'easeInOutSine',
+        duration: 1500,
+        delay: function(el, i) { return i * 250 },
+        direction: 'alternate',
+        loop: false
+    });
+}
+
+animate();
